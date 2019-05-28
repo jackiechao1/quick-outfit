bf548cf58a1b761a0f721491571fc782b6ebca79
Jackie Chao <jackie.chao1@hotmail.com>
2019-05-28 10:45:40 -0400

    added some basic methods in the controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,46 @@
 class ItemsController < ApplicationController
   def index
+  	@tops = Item.where(category: 'top')
+  	@bottoms = Item.where(category: 'bottom')
+  	@shoes = Item.where(category: 'shoes')
+  	@accessories = Item.where(category: 'accessories')
   end
+
+  def new
+  	@item = Item.new
+  end
+
+  def create
+  	@item = Item.new(item_params)
+  	if @item.save
+  		redirect_to items_path
+  	else
+  		render :new
+  	end
+  end
+
+  def edit
+		@item = Item.find(params[:id])
+  end
+
+  def update
+  	@item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to items_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  	item = Item.find(params[:id])
+  	item.destroy
+  	redirect_to items_path
+  end
+
+  private
+
+  def item_params
+  	params.require(:item).permit(:category, :color, :style)
+  end	
 end
--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -1,7 +1,9 @@
 class OutfitsController < ApplicationController
   def index
+  	@outfits = Outfit.all
   end
 
   def show
+  	@outfit = Outfit.find(params[:id])
   end
 end
