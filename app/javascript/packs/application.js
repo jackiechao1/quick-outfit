5accd740c75efea0356377424d5bc5f52b2e8844
m.t <marcantoine.tallec@gmail.com>
2019-05-29 10:53:56 -0400

    carousel done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,13 @@
 import "bootstrap";
+import "slick-carousel";
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
+$('.slick-carousel').slick({
+  infinite: true,
+  slidesToShow: 1,
+  centerMode: true,
+  arrows: false,
+
+  // slidesToScroll: 3
+});
