8421e7c76b24f724a502a8b9dcd93471475e5483
m.t <marcantoine.tallec@gmail.com>
2019-06-06 11:32:14 -0400

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
   def index
-    @tops = Item.where(category: 'top', user: current_user).order(:count)
-    @bottoms = Item.where(category: 'bottom', user: current_user).order(:count)
-    @shoes = Item.where(category: 'shoes', user: current_user).order(:count)
-    @accessories = Item.where(category: 'accessories', user: current_user).order(:count)
+    @tops = Item.where(category: 'top', user: current_user)
+    @bottoms = Item.where(category: 'bottom', user: current_user)
+    @shoes = Item.where(category: 'shoes', user: current_user)
+    @accessories = Item.where(category: 'accessories', user: current_user)
     @item = Item.new
     @outfit = Outfit.new
   end
