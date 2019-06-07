3a835ea658738134c51de1e277560cecf969a040
Jackie Chao <jackie.chao1@hotmail.com>
2019-06-07 12:45:16 -0400

    order items by created at on index page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   def index
-    @tops = Item.where(category: 'top', user: current_user)
-    @bottoms = Item.where(category: 'bottom', user: current_user)
-    @shoes = Item.where(category: 'shoes', user: current_user)
+    @tops = Item.where(category: 'top', user: current_user).order(:created_at)
+    @bottoms = Item.where(category: 'bottom', user: current_user).order(:created_at)
+    @shoes = Item.where(category: 'shoes', user: current_user).order(:created_at)
     @item = Item.new
     @outfit = Outfit.new
   end
